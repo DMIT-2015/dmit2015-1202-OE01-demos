8477d2c293bac0503c6aafedd29aee17539ec987
Sam Wu <swu@nait.ca>
2021-03-10 20:09:02 -0700

    Added NHL Division and Teams client

---
+++ dmit2015-rest-client/src/main/java/com/nhl/DivisionListController.java
@@ -0,0 +1,34 @@
+package com.nhl;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.omnifaces.util.Messages;
+import lombok.Getter;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named("currentDivisionListController")
+@ViewScoped
+public class DivisionListController implements Serializable {
+
+    @Inject
+    @RestClient
+    private NhlApiService _nhlApiService;
+
+    @Getter
+    private List<Division> divisionList;
+
+    @PostConstruct  // After @Inject is complete
+    public void init() {
+        try {
+            DivisionResponse divisionResponse = _nhlApiService.findDivisionResponse();;
+            divisionList = divisionResponse.getDivisions();
+        } catch (RuntimeException ex) {
+            Messages.addGlobalError(ex.getMessage());
+        }
+    }
+}
